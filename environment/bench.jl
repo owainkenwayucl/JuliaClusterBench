00e023e6a80c058afa305ebdaff73553ce3cafb8
Owain Kenway <o.kenway@ucl.ac.uk>
2021-08-12 13:29:01 +0100

    Benchmark script.

---
+++ environment/bench.jl
@@ -0,0 +1,33 @@
+using SystemBench, LinearAlgebra, LinearAlgebra.BLAS
+
+OMPThreads = try
+		parse(Int,ENV["OMP_NUM_THREADS"])
+	catch
+		1
+end
+
+JOBID = try
+		ENV["JOB_ID"]
+	catch
+		"LOGIN"
+end
+
+cluster = try
+		cluster=readchomp(`/shared/ucl/apps/cluster-bin/whereami`)
+	catch
+		"Unknown"
+end
+
+
+BLAS.set_num_threads(OMPThreads)
+actualtheads = BLAS.get_num_threads()
+
+fname = string(cluster, "-", JOBID, "-", OMPThreads, "-", actualtheads, ".txt")
+
+println("Running using ", actualthreads, " threads.")
+
+res = runbenchmark();
+
+savebenchmark(fname, res)
+
+
